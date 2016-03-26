c94e8cc9505d595f0431236a3a6e4e49ebc7ca4f
LeeHorobin <lee@horob.in>
2016-03-26 17:52:30 +0000

    Couple of bug fixes and some basic css

--- public/script/init.js
+++ public/script/init.js
@@ -7,6 +7,7 @@ window.onload = function(){
 	var canvas = document.getElementById('canvas');
 	snake.game.ctx = canvas.getContext('2d');
 	snake.game.ctx.lineWidth = "1";
+  snake.game.ctx.fillStyle = "#30F00C"
 
 	// Call snake.game.keyDown() upon detecting a key press
 	document.addEventListener('keydown', snake.game.keyDown, false);
